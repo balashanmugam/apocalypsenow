eb27010ff9e81b6babf17d8ed901511b86653c03
balashanmugam <balashanmugam@outlook.com>
2017-07-13 12:01:37 +0530

    Initial Setup, incomplete GIT

---
+++ Apocalypse Now/main.cpp
@@ -0,0 +1,11 @@
+// A project by Bala Shanmugam M and Kasi Viswanathan M.
+//Copyrights 103 Studios 2017
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	cout << "Hello world!" << endl;
+	return 0;
+}
